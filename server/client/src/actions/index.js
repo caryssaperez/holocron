9ec9337d49d4f756a17376013437ac29ac85bd60
Caryssa Perez <caryssaperez@gmail.com>
2018-04-23 18:13:02 -0400

    Add icons and fix fetch user action creator

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -4,5 +4,5 @@ import { FETCH_USER } from '../constants/actionTypes';
 export const fetchUser = () => async dispatch => {
   const response = await axios.get('/api/current_user');
 
-  dispatch({ type: FETCH_USER, payload: response });
+  dispatch({ type: FETCH_USER, payload: response.data });
 };
