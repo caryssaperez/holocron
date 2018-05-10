a2c383198c6d18d6499622ef4c86f8d83bf83c7e
Caryssa Perez <caryssaperez@gmail.com>
2018-05-10 16:33:18 -0400

    Add reactstrap

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
 
 class Header extends Component {
   renderContent() {
